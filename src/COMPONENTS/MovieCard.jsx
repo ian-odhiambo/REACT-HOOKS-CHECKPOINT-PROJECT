447c33748780c2fb9c8b37534375a639dcca59ee
ian <odhiamboian096@gmail.com>
2026-05-20 23:06:43 +0300

    another commit

--- src/COMPONENTS/MovieCard.jsx
+++ src/COMPONENTS/MovieCard.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-function MovieCard({ movie }) {
-  return (
-    <div className="movie-card bg-white rounded-xl overflow-hidden shadow-lg">
-      <img src={movie.posterURL} alt={movie.title} className="w-full h-96 object-cover" />
-      <div className="movie-info">
-        <h3 className="text-xl font-bold mb-2 text-gray-800">{movie.title}</h3>
-        <p className="text-gray-600 mb-3 text-sm leading-relaxed">{movie.description}</p>
-        <div className="movie-rating">
-           #{movie.rating}/10
-        </div>
-      </div>
-    </div>
-  );
-}
+// function MovieCard({ movie }) {
+//   return (
+//     <div className="movie-card bg-white rounded-xl overflow-hidden shadow-lg">
+//       <img src={movie.posterURL} alt={movie.title} className="w-full h-96 object-cover" />
+//       <div className="movie-info">
+//         <h3 className="text-xl font-bold mb-2 text-gray-800">{movie.title}</h3>
+//         <p className="text-gray-600 mb-3 text-sm leading-relaxed">{movie.description}</p>
+//         <div className="movie-rating">
+//            #{movie.rating}/10
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default MovieCard;
+// export default MovieCard;
